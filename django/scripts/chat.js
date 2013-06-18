4111d11d52f5e69f573d7b46b099fa6debd5b538
traviswimer <TravisWimer@Earth>
2013-06-18 19:50:01 -0400

    Added base Javascript/CSS files for Django

---
+++ django/scripts/chat.js
@@ -0,0 +1,141 @@
+(function(){
+
+// Check for document finished loading
+$(function($){
+	chat.init();
+});
+
+
+var chat = (function(){
+
+	// URLs for sending ajax requests
+	var messageURL = "GetMessages";
+	var postMessageURL = "PostMessage";
+	var usersURL = "GetUsers";
+
+	// ID of last message recieved
+	var lastMsgId = 0;
+
+	// Adds a chat message to the chat feed
+	function addChatMessage(userid, username, message){
+		// Create contents of message
+		var userMsgName = document.createElement('div');
+		userMsgName.className = "msg-user";
+		userMsgName.innerHTML = username;
+		var userImg = document.createElement('img');
+		userImg.className = "msg-img";
+		userImg.src = "user_images/"+userid+".jpg";
+		var userMsg = document.createElement('div');
+		userMsg.className = "msg-text";
+		userMsg.innerHTML = message;
+
+		var msgDiv = document.createElement('div');
+		msgDiv.className = "msg-box";
+		msgDiv.appendChild(userMsgName);
+		msgDiv.appendChild(userImg);
+		msgDiv.appendChild(userMsg);
+
+		document.getElementById('chat-feed').appendChild(msgDiv);
+
+		// scroll to bottom
+		var feed = document.getElementById("chat-feed");
+		feed.scrollTop = feed.scrollHeight;
+	}
+
+	// adds a user to the user list
+	function addUser(userInfo){
+		$('#chatters-list').append('<img id="user-pic-'+userInfo.id+'" class="chatter" src="user_images/'+userInfo.id+'.jpg">');
+	}
+
+	// Retrieves new chat messages from the server
+	function updateChatFeed(){
+		$.get(messageURL, {lastMsgId: lastMsgId}, function(data){
+			if(data && data.messages){
+
+				// Remove loading message if response
+				if(lastMsgId === 0){
+					$('#chat-feed').html('');
+				}
+
+				var messages = data.messages;
+				if(messages.length>0){
+
+					//update last message received
+					lastMsgId = messages[0].msgId;
+
+					// Loop through all new messages
+					for(var i=messages.length-1; i>=0; i--){
+						var theMessage = messages[i];
+
+						// Add message to the feed
+						addChatMessage(theMessage.userid, theMessage.user, theMessage.text);
+					}
+				}
+
+			}
+		}, "json");
+	}
+
+	// Retrieves current chat users
+	function updateUsers(){
+		$.get(usersURL, function(data){
+			if(data && data.users){
+				var users = data.users;
+				$('#chatters-list').html("");
+				for(i=0; i<users.length; i++){
+					var theUser = users[i];
+
+					// Add user to list if not already in list
+					addUser(theUser);
+				}
+
+			}
+		}, "json");
+	}
+
+	// sends a chat message to the server
+	function postMessage(message, attempts){
+		if(typeof attempts == "undefined"){
+			attempts = 0;
+		}
+		if(attempts<5){
+			$.post(postMessageURL, {msg: message}, function(data){
+				if(!data || data.success !== true){
+					setTimeout(postMessage, 1000, message, attempts+1);
+				}
+			}, 'json');
+		}
+	}
+
+	// Initialize chat functionality
+	function init(){
+		updateChatFeed();
+		updateUsers();
+
+		setInterval(updateChatFeed, 1000);
+		setInterval(updateUsers, 5000);
+
+
+		$('#chat-post-form').submit(function(e){
+			var newMessage = $('#chat-msg-input').val();
+			$('#chat-msg-input').val('');
+			postMessage(newMessage);
+			return false;
+		});
+	}
+
+	// Return public methods
+	return {
+		init: init
+	};
+
+
+
+	function arrayDifference(array1, array2){
+		return $.grep(array1, function(x){
+			return $.inArray(x, array2) < 0;
+		});
+	}
+})();
+
+})();
